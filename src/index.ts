b04b64d9e18b4342fb60e7437ac98a90b826fba6
rafaelarissa <rafaela.larissalj@gmail.com>
2022-04-14 15:40:13 -0300

    build: adding initial libs

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import cors from "cors";
+import express, { json } from "express";
+import "express-async-errors";
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Running on " + PORT);
+});
