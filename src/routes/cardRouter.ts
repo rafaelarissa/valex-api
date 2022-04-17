e29e812dbaae79ef85a7e35621a20b97cd520d37
rafaelarissa <rafaela.larissalj@gmail.com>
2022-04-17 14:21:16 -0300

    feat: added card types on schema

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import validateApiKey from "../middlewares/validateApiKey.js";
+import validateSchemaMiddleware from "../middlewares/validateSchema.js";
+import * as cardSchema from "../schemas/cardSchema.js";
 import * as cardController from "../controllers/cardController.js";
 
 const cardRouter = Router();
 
-cardRouter.post("/card", validateApiKey, cardController.createCard);
+cardRouter.post(
+  "/card",
+  validateApiKey,
+  validateSchemaMiddleware(cardSchema),
+  cardController.createCard
+);
 
 export default cardRouter;
