b205c7cf4d4e0f230eb93c864b5d0ea45e782d45
rafaelarissa <rafaela.larissalj@gmail.com>
2022-04-18 01:12:42 -0300

    feat: required card brand master

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,5 +1,29 @@
 import * as cardRepository from "../repositories/cardRepository.js";
 import * as handleError from "../middlewares/handleErrors.js";
+import { faker } from "@faker-js/faker";
+//gerar dados do cartão aqui
+
+export function setCardNumber() {
+  let number = faker.finance.creditCardNumber("mastercard");
+  // let isNumber = valid_card(number);
+
+  // while (number[0].toString() === '6' || !isNumber) {
+  // 	number = faker.finance.creditCardNumber('mastercard');
+  // 	isNumber = valid_card(number);
+
+  // 	if (number[0].toString() === '5' && isNumber) {
+  // 		break;
+  // 	}
+  // }
+
+  // if (number[0].toString() === '5') {
+  // 	while (!isNumber) {
+  // 		isNumber = valid_credit_card(number);
+  // 	}
+  // }
+
+  return number.split("-").join("");
+}
 
 export async function searchCardByTypeAndEmployeeId(type, employeeId: number) {
   const searchedCard = await cardRepository.findByTypeAndEmployeeId(
